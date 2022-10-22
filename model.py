3f7a59b0782654304f43b875a40fcd62adcddeee
Dmitrii <dkorzh10@gmail.com>
2022-10-22 14:10:44 +0300

    model.py: updeted init

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18
+from torchvision.models import resnet18, resnet50
 
 
 class Model(nn.Module):
     def __init__(self, in_features, out_features):
         super().__init__()
+        self.model = resnet18
         
